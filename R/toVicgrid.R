a0665f870b2b89e9aa332d5a67c7e4a1883f5d19
scrogster <scrogster@gmail.com>
2018-07-08 21:45:18 +1000

    Made generic toEPSG function, with other options as special cases. Updated documentation to match.

--- R/toVicgrid.R
+++ R/toVicgrid.R
@@ -1,35 +1,55 @@
 #' Transform Spatial* objects among coordinate systems I commonly use
 #'
+#' \code{toEPSG} converts a Spatial* object to another coordinate system, based on EPSG code
+#' Actual conversion is done by \code{spTransform} from package \code{maptools}
+#'
+#' This shoukd work for any EPSG codes provided at \url{http://spatialreference.org/ref/epsg/}
+#'
+#' \code{toVicgrid(spobj)} is a convenient shortcut to convert to Vicgrid (epsg 3111)
+#'
+#' \code{toMGA55(spobj)} is a convenient shortcut to convert to MGA55
+#'
+#' \code{toMGA54(spobj)} is a convenient shortcut to convert to MGA55
+#'
+#' \code{toWGS84(spobj)} is a convenient shortcut to convert to WGS85
+#'
+#' @author Michael Scroggie
+#'
 #' @param spobj A Spatial* object.
+#' @param EPSG  An EPSG code
 #' @return A Spatial* object in the specified coordinate system.
+#' @aliases toVicgrid toMGA55 toMGA54 toWGS84
+#' @export toEPSG
 #' @export toVicgrid
 #' @export toMGA55
 #' @export toMGA54
 #' @export toWGS84
 #' @examples
 #' cities_wgs<-SpatialPointsDataFrame(coords=cities[,-1], data=data.frame(city=cities$city), proj4string=CRS("+init=epsg:4326"))
-#' citiesVG<-toVicgrid(cities_wgs)
+#' citiesVG<-toEPSG(cities_wgs, 3111)
 #' plot(citiesVG, xlab="Easting", ylab="Northing", main="Victorian cities in Vicgrid coordinate system")
 #' axis(1)
 #' axis(2)
 #' box()
+toEPSG <-function(spobj, EPSG=3111){
+	epsgstring<-paste0("+init=epsg:",EPSG)
+	out<-spTransform(spobj, CRS(epsgstring))
+	return(out)
+}
+
 toVicgrid <- function(spobj) {
-  out<-spTransform(spobj, CRS("+init=epsg:3111"))
-  return(out)
+	toEPSG(spobj, 3111)
 }
 
 toMGA55 <- function(spobj) {
-	out<-spTransform(spobj, CRS("+init=epsg:283555"))
-	return(out)
+	toEPSG(spobj, 283555)
 }
 
 toMGA54 <- function(spobj) {
-	out<-spTransform(spobj, CRS("+init=epsg:283554"))
-	return(out)
+	toEPSG(spobj, 283554)
 }
 
 toWGS84 <- function(spobj) {
-	out<-spTransform(spobj, CRS("+init=epsg:4326"))
-	return(out)
+	toEPSG(spobj, 4326)
 }
 
